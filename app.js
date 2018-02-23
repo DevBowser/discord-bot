67206176c885fe84472ea1b3b14cbbe54947a90d
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-02-23 17:10:36 -0600

    Somewhat working.

--- app.js
+++ app.js
@@ -3,28 +3,35 @@ const dotenv = require('dotenv');
 // Loading Env
 dotenv.load();
 const Discord = require('discord.js')
-const bot = new Discord.Client({ autoReconnect: true });
+const bot = new Discord.Client({
+    autoReconnect: true
+});
 const discordToken = process.env.TOKEN
 const discordClientID = process.env.CLIENT_ID
 const commands = new Map();
 const commandPrefix = process.env.COMMAND_PREFIX
 
 bot.on('ready', () => {
+    bot.user.setActivity('Being MrDemonWolfs Slave`');
     console.log('Bot is online');
-});
-
-bot.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-    }
+    // Gives link to connect bot to server 
+    console.log("Please use the following link to link the discord bot to discord server" + "https://discordapp.com/api/oauth2/authorize?client_id=" + discordClientID + "&scope=bot&permissions=0")
 });
 
 // Says Hello to the user 
-bot.on('message', message =>{
-    if (message.content === command + 'hello') {
-        message.reply('Hello User');
+bot.on('message', message => {
+    if (message.author.equals(bot.user)){
+        console.log("Stop talking to your self")
     }
+    // Setups prefix and not case
+    if (!message.content.startsWith(commandPrefix)) return; 
+
+    var args = message.content.substring(commandPrefix.length).split(" ");
+    
+    switch
+
+    // if (message.content === command + 'hello') {
+    //     message.reply('Hello User');
+    // }
 })
-// Gives link to connect bot to server 
-console.log("Please use the following link to link the discord bot to discord server" + "https://discordapp.com/api/oauth2/authorize?client_id=" + discordClientID + "&scope=bot&permissions=0")
 bot.login(discordToken);
