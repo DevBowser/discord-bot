54dd0914051fdfebd369ffdc595c20b5bead4966
Nathan Henniges <demonwolf@demonwolfdev.com>
2018-02-23 23:10:32 -0600

    Updates.

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Load config
+const config = require('./config.json')
+
 // Impout
 const dotenv = require('dotenv');
 const chalk = require('chalk');
@@ -5,21 +8,23 @@ const Discord = require('discord.js')
 const bot = new Discord.Client({
     autoReconnect: true
 });
-// Loading Env
-dotenv.load();
-const discordToken = process.env.TOKEN
-const discordClientID = process.env.CLIENT_ID
-const commandPrefix = process.env.COMMAND_PREFIX
-const botActivity = process.env.BOT_ACTIVITY
+const discordToken = config.TOKEN
+const commandPrefix = config.COMMAND_PREFIX
+const botActivity = config.BOT_ACTIVITY
 const log = console.log
 const error = console.error
 
-bot.on('ready', () => {
+bot.on('ready', async () => {
     bot.user.setActivity(botActivity);
-    ``
-    log(chalk.green.bold('Bot is online'));
+
+    log(chalk.green.bold('Bot is ready!' + bot.user.username));
     // Gives link to connect bot to server 
-    log("Please use the following link to link the discord bot to discord server" + "https://discordapp.com/api/oauth2/authorize?client_id=" + discordClientID + "&scope=bot&permissions=0")
+    try {
+        let link = await bot.generateInvite(["ADMINISTRATOR"]);
+        log("Please use the following link to invite the discord bot to your server " + link);
+    } catch (e) {
+        error(e.stack);
+    }
 });
 
 // Says Hello to the user 
@@ -28,20 +33,32 @@ bot.on('message', message => {
     // Setups prefix and not case
     if (!message.content.startsWith(commandPrefix)) return;
 
-    var args = message.content.substring(commandPrefix.length).split(" ");
+    // TODO: Does this actually fetch the args correctly?
+    const args = message.content.slice(commandPrefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
 
-    switch (args[0].toLowerCase()) {
-        case "ping":
-            message.channel.send("Pong!");
-            break;
-        case "streamer":
-            message.member.addRole(message.guild.roles.find("name", "Streamer"));
-            error()
-            break;
+
+    // Ping Command works but its not fetching any args.
+    if (command === 'ping') {
+        message.channel.send('Pong!');
+    } else
+    if (command === 'blah') {
+        message.channel.send('Meh.');
     }
 
-    // if (message.content === command + 'hello') {
-    //     message.reply('Hello User');
-    // }
+    // For the future you can change this to grab from a database so you can change the allowed roles
+    // in the future without having to do a code change
+    const ALLOWED_ROLES = ['streamer', 'youtuber'];
+    if (command === "addRole") {
+        let role = args[0]; // Remember arrays are 0-based!.
+        log(role);
+        const canAdd = ALLOWED_ROLES.includes(role.toLowerCase());
+
+        if (canAdd) {
+            message.member.addRole(message.guild.roles.find("name", role));
+        } else {
+            log("Fail to update role.")
+        }
+    }
 })
 bot.login(discordToken);
